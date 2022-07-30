074d6b819658514a56b6a029cadc3fbcdbfa14a7
Xun <3020284554@qq.com>
2022-07-30 21:35:57 +0800

    完成吸顶导航

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,6 +3,8 @@
   <AppNavbar/>
   <!-- 头部组件 -->
   <AppHeader/>
+    <!-- 吸顶头部 -->
+  <AppHeaderSticky/>
   <!-- 内容容器 -->
   <div class="app-body">
     <!-- 二级路由 -->
@@ -17,8 +19,9 @@ import AppNavbar from '@/components/app-navbar.vue'
 import AppHeader from '@/components/app-header.vue'
 import AppFooter from '../components/app-footer.vue'
 import { useStore } from 'vuex'
+import AppHeaderSticky from '@/components/app-header-sticky.vue'
 export default {
-  components: { AppNavbar, AppHeader, AppFooter },
+  components: { AppNavbar, AppHeader, AppFooter, AppHeaderSticky },
   setup () {
     // 获取分类数据
     const store = useStore()
