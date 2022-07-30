9af554fab393e912292d8893586aaed1fac79625
Xun <3020284554@qq.com>
2022-07-30 10:30:44 +0800

    顶部,头部,底部组件完成

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 1.重置样式库
+import 'normalize.css'
+// 2.自己样式的重置样式
+import '@/assets/styles/common.less'
 
 createApp(App).use(store).use(router).mount('#app')
