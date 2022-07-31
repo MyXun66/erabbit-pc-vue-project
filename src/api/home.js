f7756314ebc5af7749befd9335f81f4c6a0984ec
Xun <3020284554@qq.com>
2022-07-31 22:10:50 +0800

    首页左侧导航基本完成，完成了骨架组件

---
+++ src/api/home.js
@@ -0,0 +1,13 @@
+// 首页相关api函数
+ 
+import request from "@/utils/request"
+
+// 获取品牌
+/**
+ * 
+ * @param {Integer} limit 品牌个数
+ * @returns 品牌数据
+ */
+export const findBrand = (limit = 6) => {
+    return request('/home/brand', 'get', {limit})
+}
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 导入ui组件库
+import UI from '@/components/library'
+
 // 1.重置样式库
 import 'normalize.css'
 // 2.自己样式的重置样式
 import '@/assets/styles/common.less'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(UI).mount('#app')
