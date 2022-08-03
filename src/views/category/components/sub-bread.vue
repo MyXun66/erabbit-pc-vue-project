f95ff45fa8f40b69f16c511026130c111aaeb738
Xun <3020284554@qq.com>
2022-08-03 10:51:13 +0800

    二级目录分类筛选以及面包屑显示完成

---
+++ src/views/category/components/sub-bread.vue
@@ -0,0 +1,47 @@
+<template>
+  <XtxBread>
+    <XtxBreadItem to="/">首页</XtxBreadItem>
+    <XtxBreadItem v-if="category.top" :to="`/category/${category.top.id}`">{{category.top.name}}</XtxBreadItem>
+    <Transition name="fade-right" mode="out-in">
+      <XtxBreadItem v-if="category.sub" :key="category.sub.id">{{category.sub.name}}</XtxBreadItem>
+    </Transition>
+    
+  </XtxBread>
+</template>
+<script>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+// import { useStore } from 'vuex'
+// import { useRoute } from 'vue-router'
+
+
+// import { computed } from 'vue'
+export default {
+  name: 'SubBread',
+  setup () {
+    // 通过计算属性从vuex中获取顶级和二级类目信息
+    // 数据对象：{top:{id,name},sub:{id,name}}
+    const route = useRoute()
+    const store = useStore()
+    const category = computed(()=>{
+        const cate = {}
+        // 获取数据逻辑
+        store.state.category.list.forEach(top => {
+            if (top.children) {
+                const sub = top.children.find(sub => sub.id === route.params.id)
+                if (sub) {
+                    cate.top = {id: top.id, name: top.name}
+                    cate.sub = { id: sub.id, name: sub.name }
+                }
+            }
+        })
+
+        return cate
+    })
+
+    return { category }
+  }
+}
+</script>
+<style scoped lang="less"></style>
