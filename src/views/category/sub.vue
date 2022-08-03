075f04e42fb4687d786666ce3de9734ce86541eb
Xun <3020284554@qq.com>
2022-08-03 11:44:03 +0800

    二级类目，排序组件完成

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -5,19 +5,37 @@
       <SubBread/>
       <!-- 筛选区 -->
       <SubFilter/>
+      <!-- 结果区域 -->
+      <div class="goods-list">
+        <!-- 排序 -->
+        <SubSort/>
+        <!-- 列表 -->
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 import SubBread from './components/sub-bread.vue';
 import SubFilter from './components/sub-filter.vue';
+import SubSort from './components/sub-sort.vue';
 export default {
     name: "SubCategory",
-    components: { SubBread, SubFilter }
+    components: { SubBread, SubFilter, SubSort },
+    setup () {
+      const isAllChecked = ref(true)
+
+      return { isAllChecked }
+
+    }
 }
 </script>
 
-<style scoped lang="less">
-
+<style scoped lang='less'>
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+}
 </style>
