54729bf5c64c45aafac6be67e9c9c31c614f1756
Xun <3020284554@qq.com>
2022-08-03 15:11:56 +0800

    二级类目中数据加载完成

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -1,34 +1,84 @@
 <template>
-  <div class="sub-category">
+  <div class='sub-category'>
     <div class="container">
       <!-- 面包屑 -->
-      <SubBread/>
+      <SubBread />
       <!-- 筛选区 -->
-      <SubFilter/>
+      <SubFilter />
       <!-- 结果区域 -->
       <div class="goods-list">
         <!-- 排序 -->
-        <SubSort/>
+        <SubSort />
         <!-- 列表 -->
+        <ul>
+          <li v-for="item in goodsList" :key="item.id" >
+            <GoodsItem :goods="item" />
+          </li>
+        </ul>
+        <!-- 加载 -->
+        <XtxInfiniteLoading :loading="loading" :finished="finished" @infinite="getData" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-import SubBread from './components/sub-bread.vue';
-import SubFilter from './components/sub-filter.vue';
-import SubSort from './components/sub-sort.vue';
+import SubBread from './components/sub-bread'
+import SubFilter from './components/sub-filter'
+import SubSort from './components/sub-sort'
+import GoodsItem from './components/goods-item'
+import { ref, watch } from 'vue'
+import { findSubCategoryGoods } from '@/api/category'
+import { useRoute } from 'vue-router'
 export default {
-    name: "SubCategory",
-    components: { SubBread, SubFilter, SubSort },
-    setup () {
-      const isAllChecked = ref(true)
+  name: 'SubCategory',
+  components: { SubBread, SubFilter, SubSort, GoodsItem },
+  setup () {
+    // 1. 基础布局
+    // 2. 无限加载组件
+    // 3. 动态加载数据且渲染
+    // 4. 任何筛选条件变化需要更新列表
+    const route = useRoute()
 
-      return { isAllChecked }
-
+    const loading = ref(false)
+    const finished = ref(false)
+    const goodsList = ref([])
+    // 查询参数
+    let reqParams = {
+      page: 1,
+      pageSize: 20
     }
+    // 获取数据函数
+    const getData = () => {
+      loading.value = true
+      reqParams.categoryId = route.params.id
+      findSubCategoryGoods(reqParams).then(({ result }) => {
+        if (result.items.length) {
+          goodsList.value.push(...result.items)
+          reqParams.page++
+        } else {
+          // 加载完毕
+          finished.value = true
+        }
+        // 请求结束
+        loading.value = false
+      })
+    }
+
+    // 切换二级分类重新加载
+    watch(() => route.params.id, (newVal) => {
+      if (newVal && route.path === ('/category/sub/' + newVal)) {
+        goodsList.value = [] // 商品列表为空，进入可视区，触发无限加载组件获取数据
+        reqParams = {
+          page: 1,
+          pageSize: 20
+        }
+        finished.value = false
+      }
+    })
+
+    return { loading, finished, goodsList, getData }
+  }
 }
 </script>
 
@@ -37,5 +87,17 @@ export default {
   background: #fff;
   padding: 0 25px;
   margin-top: 25px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5px;
+    li {
+      margin-right: 20px;
+      margin-bottom: 20px;
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
 }
 </style>
