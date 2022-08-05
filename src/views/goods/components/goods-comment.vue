8b3abfc68ee1e330c02817ed95c164fe4515c82f
Xun <3020284554@qq.com>
2022-08-05 22:23:10 +0800

    分页组件完成

--- src/views/goods/components/goods-comment.vue
+++ src/views/goods/components/goods-comment.vue
@@ -16,7 +16,7 @@
         </div>
       </div>
     </div>
-    <div class="sort">
+    <div class="sort" v-if="commentInfo">
       <span>排序：</span>
       <a @click="changeSort(null)" href="javascript:;" :class="{active: reqParams.sortField===null}">默认</a>
       <a @click="changeSort('createTime')" href="javascript:;" :class="{active: reqParams.sortField==='createTime'}">最新</a>
@@ -36,6 +36,8 @@
             <span class="attr">{{formatSpecs(item.orderInfo.specs)}}</span>
           </div>
           <div class="text">{{item.content}}</div>
+          <!-- 评论图片 -->
+          <GoodsCommentImage v-if="item.pictures.length" :pictures="item.pictures"/>
           <div class="time">
             <span>{{item.createTime}}</span>
             <span class="zan"><i class="iconfont icon-dianzan"></i>{{item.praiseCount}}</span>
@@ -43,91 +45,97 @@
         </div>
       </div>
     </div>
+    <!-- 分页组件 -->
+    <XtxPagination v-if="total" :total="total" :page-size="reqParams.pageSize" :current-page="reqParams.page" @current-change="changePagerFn"/>
   </div>
 </template>
 <script>
 import { inject, reactive, ref, watch } from 'vue'
 import { findGoodsCommentInfo, findGoodsCommentList } from '@/api/product'
+import GoodsCommentImage from './goods-comment-image.vue'
 export default {
-  name: 'GoodsComment',
-  setup () {
-    // 评价信息
-    const commentInfo = ref(null)
-    const goods = inject('goods')
-    findGoodsCommentInfo(goods.value.id).then(data => {
-      // 设置数据之前，tags数组追加 有图tag，全部评价tag
-      data.result.tags.unshift({
-        title: '有图',
-        tagCount: data.result.hasPictureCount,
-        type: 'img'
-      })
-      data.result.tags.unshift({
-        title: '全部评价',
-        tagCount: data.result.evaluateCount,
-        type: 'all'
-      })
-      commentInfo.value = data.result
-    })
+    name: "GoodsComment",
+    components: { GoodsCommentImage },
+    setup() {
+        // 评价信息
+        const commentInfo = ref(null);
+        const goods = inject("goods");
+        findGoodsCommentInfo(goods.value.id).then(data => {
+            // 设置数据之前，tags数组追加 有图tag，全部评价tag
+            data.result.tags.unshift({
+                title: "有图",
+                tagCount: data.result.hasPictureCount,
+                type: "img"
+            });
+            data.result.tags.unshift({
+                title: "全部评价",
+                tagCount: data.result.evaluateCount,
+                type: "all"
+            });
+            commentInfo.value = data.result;
+        });
+        // 激活tag
+        const currentTagIndex = ref(0);
+        const changeTag = (i) => {
+            currentTagIndex.value = i;
+            // 点击tag的时候修改筛选条件
+            const tag = commentInfo.value.tags[i];
+            // 情况1：全部评价
+            // 情况2：有图
+            // 情况3：正常tag
+            if (tag.type === "all") {
+                reqParams.hasPicture = null;
+                reqParams.tag = null;
+            }
+            else if (tag.type === "img") {
+                reqParams.hasPicture = true;
+                reqParams.tag = null;
+            }
+            else {
+                reqParams.hasPicture = null;
+                reqParams.tag = tag.title;
+            }
+            // 重置页码1
+            reqParams.page = 1;
+        };
+        // 点击排序
+        const changeSort = (sortField) => {
+            reqParams.sortField = sortField;
+            // 重置页码1
+            reqParams.page = 1;
+        };
+        // 准备筛选条件数据
+        const reqParams = reactive({
+            page: 1,
+            pageSize: 10,
+            hasPicture: null,
+            tag: null,
+            // 排序方式：praiseCount 热度  createTime 最新
+            sortField: null
+        });
+        // 初始化需要发送请求，筛选条件改变也要发请求
+        const commentList = ref([])
+        const total = ref(0)
+        watch(reqParams, () => {
+            findGoodsCommentList(goods.value.id, reqParams).then(data => {
+                commentList.value = data.result.items
+                total.value = data.result.counts
+            });
+        }, { immediate: true });
+        // 定义转换数据的函数（对应vue2.0的过滤器）
+        const formatSpecs = (specs) => {
+            return specs.reduce((p, c) => `${p} ${c.name}：${c.nameValue}`, "").trim()
+        };
+        const formatNickname = (nickname) => {
+            return nickname.substr(0, 1) + "****" + nickname.substr(-1);
+        };
+        // 实现分页切换
+        const changePagerFn = (newPage) => {
+          reqParams.page = newPage
+        }
 
-    // 激活tag
-    const currentTagIndex = ref(0)
-    const changeTag = (i) => {
-      currentTagIndex.value = i
-      // 点击tag的时候修改筛选条件
-      const tag = commentInfo.value.tags[i]
-      // 情况1：全部评价
-      // 情况2：有图
-      // 情况3：正常tag
-      if (tag.type === 'all') {
-        reqParams.hasPicture = null
-        reqParams.tag = null
-      } else if (tag.type === 'img') {
-        reqParams.hasPicture = true
-        reqParams.tag = null
-      } else {
-        reqParams.hasPicture = null
-        reqParams.tag = tag.title
-      }
-      // 重置页码1
-      reqParams.page = 1
+        return { commentInfo, currentTagIndex, changeTag, reqParams, commentList, changeSort, formatSpecs, formatNickname, total, changePagerFn }
     }
-
-    // 点击排序
-    const changeSort = (sortField) => {
-      reqParams.sortField = sortField
-      // 重置页码1
-      reqParams.page = 1
-    }
-
-    // 准备筛选条件数据
-    const reqParams = reactive({
-      page: 1,
-      pageSize: 10,
-      hasPicture: null,
-      tag: null,
-      // 排序方式：praiseCount 热度  createTime 最新
-      sortField: null
-    })
-
-    // 初始化需要发送请求，筛选条件改变也要发请求
-    const commentList = ref([])
-    watch(reqParams, () => {
-      findGoodsCommentList(goods.id, reqParams).then(data => {
-        commentList.value = data.result.items
-      })
-    },{immediate:true})
-
-    // 定义转换数据的函数（对应vue2.0的过滤器）
-    const formatSpecs = (specs) => {
-      return specs.reduce((p, c) => `${p} ${c.name}：${c.nameValue}`, '').trim()
-    }
-    const formatNickname = (nickname) => {
-      return nickname.substr(0, 1) + '****' + nickname.substr(-1)
-    }
-
-
-    return { commentInfo, currentTagIndex, changeTag, reqParams, commentList, changeSort, formatSpecs, formatNickname }
-  }
 }
 </script>
 <style scoped lang="less">
