3904443492a6f3d31f76e8c9906dd3e34b9b7fe9
Xun <3020284554@qq.com>
2022-08-06 17:20:18 +0800

    完成登录页面及表单验证

--- src/router/index.js
+++ src/router/index.js
@@ -5,23 +5,22 @@ const Home = () => import('@/views/home/index')
 const TopCategory = () => import('@/views/category/index')
 const SubCategory = () => import('@/views/category/sub')
 const Goods = () => import('@/views/goods/index')
+const Login = () => import('@/views/login/index')
 
 // 路由规则
 const routes = [
   // 一级路由布局容器
   {
-    path: '/',
-    component: Layout,
+    path: '/', component: Layout,
     children: [
-      {
-        path: '/',
-        component: Home
-      },
+      {path: '/', component: Home },
       {path: '/category/:id', component: TopCategory},
       {path: '/category/sub/:id', component: SubCategory},
-      { path: '/product/:id', component: Goods }
+      {path: '/product/:id', component: Goods }
     ]
-  }
+  },
+  {path: '/login', component: Login }
+
 ]
 
 const router = createRouter({
