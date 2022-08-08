17b94134dd1f21dbd8d1327723834f6676557277
Xun <3020284554@qq.com>
2022-08-08 22:18:34 +0800

    结算页面布局完成，dialog对话框组件封装完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import store from '@/store'
 
 const Layout = () => import('@/views/Layout')
 const Home = () => import('@/views/home/index')
@@ -7,6 +8,7 @@ const SubCategory = () => import('@/views/category/sub')
 const Goods = () => import('@/views/goods/index')
 const Login = () => import('@/views/login/index')
 const Cart = () => import('@/views/cart/index')
+const PayCheckout = () => import('@/views/member/pay/checkout')
 
 // 路由规则
 const routes = [
@@ -18,7 +20,8 @@ const routes = [
       {path: '/category/:id', component: TopCategory},
       {path: '/category/sub/:id', component: SubCategory},
       {path: '/product/:id', component: Goods },
-      {path: '/cart', component: Cart }
+      {path: '/cart', component: Cart },
+      { path: '/member/checkout', component: PayCheckout }
     ]
   },
   {path: '/login', component: Login }
@@ -33,6 +36,16 @@ const router = createRouter({
   scrollBehavior () {
     return { left: 0, top: 0 }
   }
+})
+
+// 前置导航守卫
+router.beforeEach((to, from, next) => {
+  // 需要登录的路由：地址是以 /member 开头
+  const { profile } = store.state.user
+  if (!profile.token && to.path.startsWith('/member')) {
+    return next('/login?redirectUrl=' + encodeURIComponent(to.fullPath))
+  }
+  next()
 })
 
 export default router
